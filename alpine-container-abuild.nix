0c7c01b07986c433dd929c517bee2e53b80d0135
maxice8 <thinkabit.ukim@gmail.com>
2022-03-05 18:52:51 -0300

    alpine-container-abuild: hardcode image path

--- alpine-container-abuild.nix
+++ alpine-container-abuild.nix
@@ -41,5 +41,5 @@ ${pkgs.podman}/bin/podman run --tty --interactive \
   --userns=keep-id \
   --rm \
   --workdir /home/builder/aports/"''${PWD#*/aports/}" \
-  alpine-container-abuild:edge-$DABUILD_ARCH "$@"
+  docker.io/maxice8/alpine-container-abuild:edge-$DABUILD_ARCH "$@"
 ''
