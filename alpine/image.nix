92f30da040b4b81f4ee2d9294b63f89660f25bf6
maxice8 <thinkabit.ukim@gmail.com>
2022-03-10 18:38:37 -0300

    alpine: add apkg-diff as a possible entrypoint

--- alpine/image.nix
+++ alpine/image.nix
@@ -1,11 +1,13 @@
 { pkgs, lib }:
 let
-  alpine-base = pkgs.dockerTools.pullImage {
-    imageName = "docker.io/maxice8/alpine-container-abuild";
-    imageDigest = "sha256:294015c79d86973f16bcceeaa3ca60b4dd136117120c751353adce889eb4fcf3";
-    sha256 = "0mkmq7kxq8v8f15b23sfpnl1wdphichpqnsbslj9vrp3si6cf0sq";
-    finalImageTag = "edge-x86_64";
-  };
+  alpine-base = pkgs.dockerTools.pullImage
+    {
+      imageName = "docker.io/maxice8/alpine-container-abuild";
+      imageDigest = "sha256:c60451c002229fb3b951d6f7f5f8d0b8499b68a8a0295ae1320a16ed3a185f59";
+      sha256 = "03jf4cw52x2y9yl82l1rrb8rrd9p7rrk49akpqy4nxsny9xcb6s4";
+      finalImageName = "docker.io/maxice8/alpine-container-abuild";
+      finalImageTag = "edge-x86_64";
+    };
 in
 pkgs.dockerTools.buildImage {
   name = "alpine-container-abuild";
