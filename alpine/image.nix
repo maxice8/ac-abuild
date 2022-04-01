76d36d54c80a1314ed6b81f0b343b753a43d8f62
maxice8 <thinkabit.ukim@gmail.com>
2022-04-01 16:52:45 -0300

    feat(image): update

--- alpine/image.nix
+++ alpine/image.nix
@@ -3,8 +3,8 @@ let
   alpine-base = pkgs.dockerTools.pullImage
     {
       imageName = "docker.io/maxice8/alpine-container-abuild";
-      imageDigest = "sha256:e53f5194534e247a6918796b57cb6993c2ba70d85dc193fc3a0bf70f1e5c37e8";
-      sha256 = "10gb0p3kkkl622sbn0fnvvxqgy262rr5xy23h5106cqf88bi2i1m";
+      imageDigest = "sha256:4a3aadafdce5acf2c6c0189d11a10640564883a5f64d8297afc342ba15ae564c";
+      sha256 = "00xr1zjlk1vv2wnl5k0n0g40x1a7ryp9k13md73yvlsf11k16gjs";
       finalImageName = "docker.io/maxice8/alpine-container-abuild";
       finalImageTag = "edge-x86_64";
     };
