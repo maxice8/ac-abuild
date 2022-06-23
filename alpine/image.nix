e0191d9c372a3c601aab1dfb0135cf3b45d4f02d
maxice8 <thinkabit.ukim@gmail.com>
2022-06-23 12:29:37 -0300

    remove abuild, use nixpkgs

--- alpine/image.nix
+++ alpine/image.nix
@@ -3,8 +3,8 @@ let
   alpine-base = pkgs.dockerTools.pullImage
     {
       imageName = "docker.io/maxice8/alpine-container-abuild";
-      imageDigest = "sha256:f81ba82d9b5a24adb02a92f0d6422a512b4dd228c7509179f6e9d288e0ae4f2b";
-      sha256 = "1ipy00p9sxjk56bcipq97db7y1vqmm7njflx6xadrdhsbx4q5fiy";
+      imageDigest = "sha256:571e569ae8da5034bbeeb45ba42e104457b4faca4c165cef585ab3122baad1ff";
+      sha256 = "1d5k2ak0rf069pcd3dg9156j0699cqcl510imy8l2v2qc54ah1qr";
       finalImageName = "docker.io/maxice8/alpine-container-abuild";
       finalImageTag = "edge-x86_64";
     };
--- flake.nix
+++ flake.nix
@@ -8,8 +8,6 @@
     repo = "atools";
     inputs.nixpkgs.follows = "nixpkgs";
   };
-  inputs.maxice8-nix.url = "github:maxice8/nix-overlay";
-  inputs.maxice8-nix.inputs.nixpkgs.follows = "nixpkgs";
   outputs = { self, nixpkgs, ... }:
     let
       supportedSystems = [ "x86_64-linux" "i686-linux" "aarch64-linux" ];
@@ -26,7 +24,6 @@
           ac-abuild-shell-utils = pkgs.callPackage ./shell-utils.nix {
             inherit (self.packages.${system}) alpine-container-abuild;
             inherit (self.inputs.atools.packages.${system}) atools;
-            inherit (self.inputs.maxice8-nix.packages.${system}) abuild;
           };
           default = self.packages.${system}.alpine-container-abuild;
         });
